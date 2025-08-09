ac1b6a64616bd1fbdca472dd02582cd18ae8e54c
shreya27-patil <shreyarpatil00@gmail.com>
2025-08-09 21:21:23 +0530

    Update GiftCouponCard.jsx

--- frontend/src/components/GiftCouponCard.jsx
+++ frontend/src/components/GiftCouponCard.jsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
-import { useCartStore } from "../stores/useCartStore";
+import { useCartStore } from "../stores/useCartStore";//code
 
 const GiftCouponCard = () => {
 	const [userInputCode, setUserInputCode] = useState("");
