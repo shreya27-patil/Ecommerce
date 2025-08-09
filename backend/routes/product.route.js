b0e49244a20c6e7f6f0c36fb4c6598e00cf89522
shreya27-patil <shreyarpatil00@gmail.com>
2025-08-09 21:07:15 +0530

    Update product.route.js

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -2,7 +2,7 @@ import express from "express";
 import {
 	createProduct,
 	deleteProduct,
-	getAllProducts,
+	getAllProducts,//
 	getFeaturedProducts,
 	getProductsByCategory,
 	getRecommendedProducts,
