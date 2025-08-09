4a977ac15026665e0265fa395eca92b95debf002
shreya27-patil <shreyarpatil00@gmail.com>
2025-08-09 21:24:59 +0530

    Update CartPage.jsx

--- frontend/src/pages/CartPage.jsx
+++ frontend/src/pages/CartPage.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";//code
 import { useCartStore } from "../stores/useCartStore";
 import { motion } from "framer-motion";
 import { ShoppingCart } from "lucide-react";
