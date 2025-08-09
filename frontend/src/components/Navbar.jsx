d5b7aa7bbc780552a5ab977e49eeee07991d7509
shreya27-patil <shreyarpatil00@gmail.com>
2025-08-09 21:22:05 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { useCartStore } from "../stores/useCartStore";
 const Navbar = () => {
 	const { user, logout } = useUserStore();
 	const isAdmin = user?.role === "admin";
-	const { cart } = useCartStore();
+	const { cart } = useCartStore();//code
 
 	return (
 		<header className='fixed top-0 left-0 w-full bg-gray-900 bg-opacity-90 backdrop-blur-md shadow-lg z-40 transition-all duration-300 border-b border-emerald-800'>
