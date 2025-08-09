6f6ba5de9fde368dab16ffa9f4cf2714435ccbe5
shreya27-patil <shreyarpatil00@gmail.com>
2025-08-09 21:29:22 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";//code
 
 import HomePage from "./pages/HomePage";
 import SignUpPage from "./pages/SignUpPage";
