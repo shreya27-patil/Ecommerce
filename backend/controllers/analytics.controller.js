379f981c63f02f432cd36b99e11629e1e3426156
shreya27-patil <shreyarpatil00@gmail.com>
2025-08-09 21:09:05 +0530

    Update analytics.controller.js

--- backend/controllers/analytics.controller.js
+++ backend/controllers/analytics.controller.js
@@ -4,7 +4,7 @@ import User from "../models/user.model.js";
 
 export const getAnalyticsData = async () => {
 	const totalUsers = await User.countDocuments();
-	const totalProducts = await Product.countDocuments();
+	const totalProducts = await Product.countDocuments();//code
 
 	const salesData = await Order.aggregate([
 		{
