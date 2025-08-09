f424b12e0629adf3efa611f20f3d0a2b7a222ac2
shreya27-patil <shreyarpatil00@gmail.com>
2025-08-09 20:58:09 +0530

    Update cart.controller.js

--- backend/controllers/cart.controller.js
+++ backend/controllers/cart.controller.js
@@ -4,7 +4,7 @@ export const getCartProducts = async (req, res) => {
 	try {
 		const products = await Product.find({ _id: { $in: req.user.cartItems } });
 
-		// add quantity for each product
+		// add quantity for each product//code
 		const cartItems = products.map((product) => {
 			const item = req.user.cartItems.find((cartItem) => cartItem.id === product.id);
 			return { ...product.toJSON(), quantity: item.quantity };
